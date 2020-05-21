f99be98178cfc1e80c333737922181ea65b66162
root <root@6f6a5fbd3c35>
2020-05-21 15:41:18 +0000

    ArticleControoler dodany, routes.yml

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,7 +4,14 @@
 namespace App\Controller;
 
 
+use Symfony\Component\HttpFoundation\Response;
+
 class ArticleController
 {
+    public function homepage()
+    {
+        return new Response('Demo');
+
+    }
 
 }
