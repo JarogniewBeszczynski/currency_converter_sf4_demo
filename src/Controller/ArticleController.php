22b8d6ddc3b9d771b72cb7a2e4f721303ef2defb
j.j.beszczynski <j.beszczynski@gmail.com>
2020-05-21 18:25:36 +0200

    new view

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,14 +4,21 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     public function homepage()
     {
         return new Response('Demo');
+    }
 
+    public function news($slug)
+    {
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+        ]);
     }
 
 }
